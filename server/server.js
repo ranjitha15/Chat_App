9d6b524a0730194f8cfec7b93c30ea6e963c879b
Ranjitha Palanichamy <ranjithasivakami@gmail.com>
2017-11-21 12:58:57 +0100

    Greet new users and inform others

--- server/server.js
+++ server/server.js
@@ -10,14 +10,35 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log("New user connected");
-
-socket.on('createMessage',(message)=>{
-    console.log('createMessage',message);
-    io.emit('newMessage',{
-        from:message.from,
-        text:message.text,
+//--Welcome message from admin to the new user--//
+    socket.emit('newMessage',{
+        from: "Admin",
+        text: "Welcome to the Chat room",
         createdAt: new Date().getTime()
+
     });
+//--Informing other users that a new user has joined--//
+    socket.broadcast.emit('newMessage',{
+        from: "Admin",
+        text: "New User has joined",
+        createdAt: new Date().getTime()
+
+    });
+
+
+    socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });
+//--Send message to everybody except the sender--//
+    /*socket.broadcast.emit('newMessage',{
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+    });*/
 
 });
 
