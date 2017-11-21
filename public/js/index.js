da8f4ef3427ac9a820c736c9d620f00f458a065d
Ranjitha Palanichamy <ranjithasivakami@gmail.com>
2017-11-21 12:37:41 +0100

    Broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+var socket = io();
+socket.on('connect',function(){
+    console.log("connected to server");
+});
+socket.on('disconnect',function(){
+    console.log("disconnected from server");
+});
+socket.on('newMessage',function(message){
+    console.log("Message received",message);
+});
