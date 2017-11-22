83f299321c0555bd59ec6a26f2f3b2313ba1096e
Ranjitha Palanichamy <ranjithasivakami@gmail.com>
2017-11-22 10:41:17 +0100

    Send message and current location

--- public/js/index.js
+++ public/js/index.js
@@ -23,12 +23,12 @@ socket.on('newLocationMessage',function(message){
 
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
-
+var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage',{
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     },function(){
-
+        messageTextbox.val(''); //clear its value
     });
 });
 //.Geolocation--//
@@ -37,13 +37,16 @@ locationButton.on('click', function(){
     if(!navigator.geolocation){ //geolocation API
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled','disabled');
     navigator.geolocation.getCurrentPosition(function(position){
+        //locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
 
     },function(){
+        //locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
